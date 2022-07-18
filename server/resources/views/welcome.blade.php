0e0799f38331a93c78067311e5f192bcbfe31f07
Yuta Sawamura <>
2022-07-18 21:05:24 +0900

    Add: headの共通化

--- server/resources/views/welcome.blade.php
+++ server/resources/views/welcome.blade.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-</head>
-
-<body>
-    <p class="text-green-300">澤村</p>
-</body>
-
-</html>
+@extends('layouts.app')
+@section('content')
+<p class="text-2xl text-sky-400">澤村</p>
+@endsection

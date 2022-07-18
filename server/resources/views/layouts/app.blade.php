e3deacfdca1bf131d9159d408485094a366375a7
Yuta Sawamura <>
2022-07-18 21:50:22 +0900

    Add: ヘッダーの作成

--- server/resources/views/layouts/app.blade.php
+++ server/resources/views/layouts/app.blade.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ja">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+  <title>Laravel</title>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <script src="https://cdn.tailwindcss.com"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <!--  BEGIN CONTENT AREA  -->
-    @yield('content')
-    <!--  END CONTENT AREA  -->
+  <!--  BEGIN NAVBAR  -->
+  @include('layouts._header')
+  <!--  END NAVBAR  -->
+
+  <!--  BEGIN CONTENT AREA  -->
+  @yield('content')
+  <!--  END CONTENT AREA  -->
 </body>
 
 </html>
